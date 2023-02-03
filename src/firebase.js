e8b6142c5fd0e8a8b22b7c20100bd03216778fda
pththanh <phamtruonghoaithanh.kols@gmail.com>
2023-02-04 01:43:26 +0700

    initial project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC76JmvoyWwUVPvKWKlogbN3Gldve0qxV8",
+  authDomain: "netflix-clone-e266c.firebaseapp.com",
+  projectId: "netflix-clone-e266c",
+  storageBucket: "netflix-clone-e266c.appspot.com",
+  messagingSenderId: "516241954711",
+  appId: "1:516241954711:web:72c199292399db4c9656d2",
+};
